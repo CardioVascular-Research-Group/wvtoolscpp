bf7dc15142838215a89803a296385fef5b94b3ca
Absox <rliu14@jhu.edu>
2016-06-28 17:05:09 +0000

    Added dependency on json parser.

--- test/RestApiTest.cpp
+++ test/RestApiTest.cpp
@@ -5,6 +5,9 @@
 #include <gtest/gtest.h>
 
 #include "restclient-cpp/restclient.h"
+#include "json.hpp"
+
+using nlohmann::json;
 
 using RestClient::Response;
 
@@ -16,8 +19,11 @@ TEST(rest_api_tests, test_version) {
     Response response = RestClient::get("http://localhost:3001/api/version");
 
     EXPECT_EQ(200, response.code);
-    cout << response.body << endl;
-    cout << response.code << endl;
+    EXPECT_EQ('{', response.body[0]);
 
+    auto parsed_response = json::parse(response.body);
+
+    EXPECT_STRNE(parsed_response["host"], "null");
+    EXPECT_STRNE(parsed_response["short_revision"], "null");
 
 }
