4e9eb6fd06ecb603f492b42a693d4c219c50e3f3
Absox <rliu14@jhu.edu>
2016-06-28 20:01:44 +0000

    Fixed import for cygwin.

--- src/util/ChecksumCalculator.h
+++ src/util/ChecksumCalculator.h
@@ -4,7 +4,7 @@
 
 #pragma once
 
-#include <zconf.h>
+#include <ctype.h>
 #include <vector>
 
 /**
--- test/TimestampTest.cpp
+++ test/TimestampTest.cpp
@@ -4,9 +4,22 @@
 
 #include <gtest/gtest.h>
 
-#include <boost/date_time.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+using namespace boost::gregorian;
+using namespace boost::posix_time;
+
+using std::cout;
+using std::endl;
 
 TEST(timestamp_test, basic_arithmetic_test) {
 
+    ptime time(date(2002, Jan, 10), hours(13) + minutes(5) + seconds(2) + milliseconds(23));
 
+    cout << endl;
+    cout << time << endl;
+    cout << time + hours(2) << endl;
+
+    cout << "timestamp " << (time - ptime(date(1970, 1, 1))).total_seconds() << endl;
 }
