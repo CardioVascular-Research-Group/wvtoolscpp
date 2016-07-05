d4191bbd60587055dfe6021b273681f26c874bc2
Absox <xabsox@gmail.com>
2016-07-05 13:48:45 -0400

    Added tsuid cache.

--- src/tsdb/TsuidCache.cpp
+++ src/tsdb/TsuidCache.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "TsuidCache.h"
+
+using std::unordered_map;
+
+TsuidCache::TsuidCache() {
+
+}
+
+inline bool TsuidCache::contains(const std::string &metric, const std::string &subject_id) {
+    if (contains_subject(metric)) return tsuids[subject_id].find(metric) != tsuids[subject_id].end();
+    return false;
+}
+
+
+inline bool TsuidCache::contains_subject(const std::string &subject_id) {
+    return tsuids.find(subject_id) != tsuids.end();
+}
+
+
+std::string TsuidCache::get_tsuid(const std::string &metric, const std::string &subject_id) {
+    if (contains(metric, subject_id)) return tsuids[subject_id][metric];
+    return "";
+}
+
+
+void TsuidCache::put(const std::string &metric, const std::string &subject_id, const std::string &tsuid) {
+    if (contains_subject(subject_id)) {
+
+    } else {
+        tsuids[subject_id] = unordered_map<std::string, std::string>();
+    }
+}
--- src/tsdb/TsuidCache.h
+++ src/tsdb/TsuidCache.h
@@ -5,9 +5,18 @@
 #ifndef WVTOOLS_TSUIDCACHE_H
 #define WVTOOLS_TSUIDCACHE_H
 
+#include <string>
+#include <unordered_map>
 
 class TsuidCache {
-
+public:
+    TsuidCache();
+    bool contains(const std::string& metric, const std::string& subject_id);
+    bool contains_subject(const std::string& subject_id);
+    std::string get_tsuid(const std::string& metric, const std::string& subject_id);
+    void put(const std::string& metric, const std::string& subject_id, const std::string& tsuid);
+private:
+    std::unordered_map<std::string, std::unordered_map<std::string, std::string>> tsuids;
 };
 
 
